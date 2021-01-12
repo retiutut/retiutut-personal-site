3174fffe157044ace055c52028f176d3917910f4
Richard Waltman <retiutut@icloud.com>
2021-01-11 18:28:55 -0600

    Deploy website - based on d33be823e6595835d2a22bddb453948ab90da5c3

---
+++ 61a27c6e.e1ae299a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{78:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/retiutut-personal-site/blog/welcome"},{"title":"Hello","permalink":"/retiutut-personal-site/blog/hello-world"},{"title":"Hola","permalink":"/retiutut-personal-site/blog/hola"}]}')}}]);
