9e6853b13efe9664b161114310eea22436ebbdaf
Richard Waltman <retiutut@icloud.com>
2026-01-21 17:07:52 -0600

    Update outdated default blog posts to newer Docusaurus spec

--- blog/2019-05-29-hello-world.md
+++ blog/2019-05-29-hello-world.md
@@ -1,16 +1,13 @@
 ---
 slug: hello-world
 title: Hello
-author: Endilie Yacop Sucipto
-author_title: Maintainer of Docusaurus
-author_url: https://github.com/endiliey
-author_image_url: https://avatars1.githubusercontent.com/u/17883920?s=460&v=4
+authors: endiliey
 tags: [hello, docusaurus]
 ---
 
 Welcome to this blog. This blog is created with [**Docusaurus 2 alpha**](https://v2.docusaurus.io/).
 
-<!--truncate-->
+{/_ truncate _/}
 
 This is a test post.
 
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,110 +1,123 @@
 module.exports = {
-  title: 'Richard Waltman',
-  tagline: 'Welcome to my personal site and blog!',
-  url: 'https://retiutut.github.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/djwave-formal-internet2favicon.ico',
-  organizationName: 'retiutut', // Usually your GitHub org/user name.
-  projectName: 'retiutut-personal-site', // Usually your repo name.
-  themeConfig: {
-    colorMode: {
-      defaultMode: 'dark',
-      disableSwitch: false,
-      respectPrefersColorScheme: false,
-    },
-    navbar: {
-      title: 'Richard Waltman',
-      logo: {
-        alt: 'Retiutut Logo',
-        src: 'img/djwave formal internet2 200x200.jpg',
-      },
-      items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/retiutut/',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/retiutut',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Richard Waltman.`,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
-  ],
+	title: 'Richard Waltman',
+	tagline: 'Welcome to my personal site and blog!',
+	url: 'https://retiutut.github.io',
+	baseUrl: '/',
+	onBrokenLinks: 'throw',
+	favicon: 'img/djwave-formal-internet2favicon.ico',
+	markdown: {
+		format: 'mdx',
+		mermaid: false,
+		preprocessor: undefined,
+		parseFrontMatter: undefined,
+		mdx1Compat: {
+			comments: false,
+			admonitions: false,
+			headingIds: false,
+		},
+		hooks: {
+			onBrokenMarkdownLinks: 'warn',
+		},
+	},
+	organizationName: 'retiutut', // Usually your GitHub org/user name.
+	projectName: 'retiutut-personal-site', // Usually your repo name.
+	themeConfig: {
+		colorMode: {
+			defaultMode: 'dark',
+			disableSwitch: false,
+			respectPrefersColorScheme: false,
+		},
+		navbar: {
+			title: 'Richard Waltman',
+			logo: {
+				alt: 'Retiutut Logo',
+				src: 'img/djwave formal internet2 200x200.jpg',
+			},
+			items: [
+				{
+					to: 'docs/',
+					activeBasePath: 'docs',
+					label: 'Docs',
+					position: 'left',
+				},
+				{ to: 'blog', label: 'Blog', position: 'left' },
+				{
+					href: 'https://github.com/retiutut/',
+					label: 'GitHub',
+					position: 'right',
+				},
+			],
+		},
+		footer: {
+			style: 'dark',
+			links: [
+				{
+					title: 'Docs',
+					items: [
+						{
+							label: 'Style Guide',
+							to: 'docs/',
+						},
+						{
+							label: 'Second Doc',
+							to: 'docs/doc2/',
+						},
+					],
+				},
+				{
+					title: 'Community',
+					items: [
+						{
+							label: 'Stack Overflow',
+							href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+						},
+						{
+							label: 'Discord',
+							href: 'https://discordapp.com/invite/docusaurus',
+						},
+						{
+							label: 'Twitter',
+							href: 'https://twitter.com/docusaurus',
+						},
+					],
+				},
+				{
+					title: 'More',
+					items: [
+						{
+							label: 'Blog',
+							to: 'blog',
+						},
+						{
+							label: 'GitHub',
+							href: 'https://github.com/retiutut',
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} Richard Waltman.`,
+		},
+	},
+	presets: [
+		[
+			'@docusaurus/preset-classic',
+			{
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					editUrl:
+						'https://github.com/facebook/docusaurus/edit/master/website/',
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					editUrl:
+						'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			},
+		],
+	],
 };
